0663734b6000295f1b42119ec8d276851c6ea712
DavitOhanj <57683628+DavitOhanj@users.noreply.github.com>
2019-11-21 23:18:19 +0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,17 +1,17 @@
-function multiplyMatrices(mat1, mat2){
-  let arr1 = mat1[0];
+function multiplyMatrices(matrix1, matrix2){
+  let arr1 = matrix1[0];
   let arr2 = [];
-  for (let i = 0; i < mat2.length; i++){
-    arr2.push(mat2[i][0]);
+  for (let i = 0; i < matrix2.length; i++){
+    arr2.push(matrix2[i][0]);
   }
   if(arr1.length === arr2.length){
-    let res = new Array(mat1.length);
-    for (let k = 0; k < mat1.length; k++) {
-      res[k] = new Array(mat2[0].length); // initialize the current row
-      for (let c = 0; c < mat2[0].length; c++) {
-        res[k][c] = 0;             // initialize the current cell
-        for (let i = 0; i < mat1[0].length; i++) {
-          res[k][c] += mat1[k][i] * mat2[i][c];
+    let res = new Array(matrix1.length);
+    for (let k = 0; k < matrix1.length; k++) {
+      res[k] = new Array(matrix2[0].length); // init matrix row
+      for (let c = 0; c < matrix2[0].length; c++) {
+        res[k][c] = 0;             // init matrix cell
+        for (let i = 0; i < matrix1[0].length; i++) {
+          res[k][c] += matrix1[k][i] * matrix2[i][c];
         }
       }
     }
