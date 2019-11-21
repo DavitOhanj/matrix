f740ccde81a6921e61fec88c38477037172081ea
DavitOhanj <57683628+DavitOhanj@users.noreply.github.com>
2019-11-21 23:11:38 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const utils = require('./utils');
 const matrix= require('./matrix');
 
-let mat = [['A', 'B', 'C'],
+let matrix = [['A', 'B', 'C'],
   ['D', 'E', 'F'],
   ['G', 'H', 'I']];
 
 
-let mat1 = [[1, 2, 3],
+let matrix1 = [[1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]];
 
-let mat2 = [[7, 6, 3],
+let matrix2 = [[7, 6, 3],
   [8, 5, 2],
   [9, 4, 1]];
 
-console.log(utils.multiplyMatrices(mat1, mat2));
-console.log(matrix.printMat(mat));
+console.log(utils.multiplyMatrices(matrix1, matrix2));
+console.log(matrix.printMat(matrix));
